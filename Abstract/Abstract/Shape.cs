fc7f49aea758eb5ccf04a2436937824daa1d9e4c
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-13 14:33:17 -0700

    init commit

---
+++ Abstract/Abstract/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    abstract class Shape
+    {
+       public string Name { get; set; }
+
+       public virtual void GetInfo()
+        {
+            Console.WriteLine($"\n This is a {Name}");
+        }
+        public abstract double Volume();
+    }
+}
